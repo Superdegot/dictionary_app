6c4b015d345f16098e3bab047427d2ca8b1e4c21
Superdegot <65909405+Superdegot@users.noreply.github.com>
2023-01-12 16:02:44 +0000

    added an element to a link

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,11 +3,13 @@ import React from "react";
 export default function Phonetic (props) {
     console.log(props.phonetic);
     return (
-        <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" >Listen</a>
-            <br/>
-            {props.phonetic.text}
-        </div>
+      <div className="Phonetic">
+        <a href={props.phonetic.audio} target="_blank" rel="noopener">
+          Listen
+        </a>
+        <br />
+        {props.phonetic.text}
+      </div>
     );
    
 }
